9950d4155e8e7c5b389db074d14680ce9c297f14
MohamedSamir72 <mosamir9792@gmail.com>
2024-07-07 22:06:50 +0300

    Handle reading frames

--- Object_Tracking_Counting_Cars/Object_Tracking_OpenCV.py
+++ Object_Tracking_Counting_Cars/Object_Tracking_OpenCV.py
@@ -1,1 +1,27 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture("video.mp4")
+
+# Check if the video file opened successfully
+if not cap.isOpened():
+    print("Error opening video file")
+
+# Read and display frames from the video
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    # To ignore the error when the video finished
+    try:
+        cv2.imshow("video", frame)
+    except:
+        print("Video finished")
+        break
+    
+    # Adjust delay (20 milliseconds here, adjust as needed)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
 
