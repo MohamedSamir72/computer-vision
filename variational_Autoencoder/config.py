5dbdd42a2b52d43e28a5332da2fe6394a28929ad
MohamedSamir72 <mosamir9792@gmail.com>
2025-10-23 22:02:27 +0300

    Modify configurations

--- variational_Autoencoder/config.py
+++ variational_Autoencoder/config.py
@@ -4,8 +4,8 @@ from dataclasses import dataclass
 class Config:
     LEARNING_RATE: float = 0.001
     BATCH_SIZE: int = 64
-    EPOCHS: int = 50
+    EPOCHS: int = 10
     DEVICE: str = 'cuda'
     RECONSTRUCTION_LOSS_WEIGHT: float = 1.0
-    KLD_LOSS_WEIGHT: float = 0.1
+    KLD_LOSS_WEIGHT: float = 1.0
     
