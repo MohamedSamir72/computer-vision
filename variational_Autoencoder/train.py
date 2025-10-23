70fdda9abcf61fdce5252b6d900eddaf94783e28
MohamedSamir72 <mosamir9792@gmail.com>
2025-10-23 22:09:23 +0300

    Modify train function

--- variational_Autoencoder/train.py
+++ variational_Autoencoder/train.py
@@ -1,20 +1,21 @@
 import torch
-from torch import nn, optim
+from torch import optim
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from config import Config
 from model import Conv_VAE
 from dataset import Map_Dataset
+from tqdm import tqdm
 
-def VAE_loss(recon_x, x, mu, logvar, reconstruction_loss_weight, kld_loss_weight):
+def VAE_loss(recon_x, x, mu, logvar, reconstruction_loss_weight=1.0, kld_loss_weight=1.0):
     # Reconstruction loss
     mse = (recon_x - x) ** 2
     mse = mse.flatten(1).sum(dim=1)
-    mse = mse.mean().item()
+    mse = mse.mean()
     
     # KL Divergence
     kl_divergence = -0.5 * torch.sum(1 + logvar - mu.pow(2) - torch.exp(logvar), dim=1)
-    kl_divergence = kl_divergence.mean().item()
+    kl_divergence = kl_divergence.mean()
 
     return (reconstruction_loss_weight * mse) + (kld_loss_weight * kl_divergence)
 
@@ -34,32 +35,37 @@ def train():
     optimizer = optim.Adam(model.parameters(), lr=config.LEARNING_RATE)
     model.train()
 
+    print("Starting Training...")
     for epoch in range(config.EPOCHS):
         total_loss = 0
-        for batch_idx, (data, _) in enumerate(train_loader):
+        for batch_idx, (data, _) in tqdm(enumerate(train_loader)):
             data = data.to(config.DEVICE)
             optimizer.zero_grad()
             recon_batch, mu, logvar = model(data)
 
-            loss = VAE_loss(recon_batch, data, mu, logvar)
+            loss = VAE_loss(recon_batch, data, mu, logvar, 
+                            config.RECONSTRUCTION_LOSS_WEIGHT, 
+                            config.KLD_LOSS_WEIGHT)
             loss.backward()
-            total_loss += loss.item()
+            total_loss += loss
             optimizer.step()
 
         print(f'Epoch {epoch+1}, Loss: {total_loss / len(train_loader.dataset)}')
 
 
 if __name__ == "__main__":
-    # train()
-    # handle random seed for reproducibility
-    torch.manual_seed(0)
+    train()
 
-    x = torch.randn((4, 1, 28, 28))
-    x_hat = torch.randn((4, 1, 28, 28))
-    mu = torch.randn((4, 2))
-    logvar = torch.randn((4, 2))
 
-    loss = VAE_loss(x_hat, x, mu, logvar,
-                    Config.RECONSTRUCTION_LOSS_WEIGHT,
-                    Config.KLD_LOSS_WEIGHT)
-    # print("Loss:", loss.item())
+    # # handle random seed for reproducibility
+    # torch.manual_seed(0)
+
+    # x = torch.randn((4, 1, 28, 28))
+    # x_hat = torch.randn((4, 1, 28, 28))
+    # mu = torch.randn((4, 2))
+    # logvar = torch.randn((4, 2))
+
+    # loss = VAE_loss(x_hat, x, mu, logvar,
+    #                 Config.RECONSTRUCTION_LOSS_WEIGHT,
+    #                 Config.KLD_LOSS_WEIGHT)
+    # # print("Loss:", loss)
