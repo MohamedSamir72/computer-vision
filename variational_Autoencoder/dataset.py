7be3accca1c1381b56ca76ed8b46be602876d32b
MohamedSamir72 <mosamir9792@gmail.com>
2025-10-23 19:19:13 +0300

    Fix the module

--- variational_Autoencoder/dataset.py
+++ variational_Autoencoder/dataset.py
@@ -11,6 +11,7 @@ class Map_Dataset(Dataset):
             transforms.ToTensor(),
             transforms.Normalize((0.5,), (0.5,))
         ])
+        
     def __len__(self):
         return len(self.data)
     
