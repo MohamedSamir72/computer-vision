76e6d4f0cf037fceb2047d589854aeb837fe00bb
MohamedSamir72 <mosamir9792@gmail.com>
2025-10-23 19:39:36 +0300

    Fix the module

--- variational_Autoencoder/dataset.py
+++ variational_Autoencoder/dataset.py
@@ -1,25 +1,39 @@
 from torchvision import transforms
 from torch.utils.data import Dataset
+from PIL import Image
+import torch
 
 class Map_Dataset(Dataset):
-    def __init__(self, data, labels):
+    def __init__(self, data, labels, transform=None):
         super().__init__()
         self.data = data
         self.labels = labels
-        self.transform = transforms.Compose([
-            transforms.Resize((32, 32)),
-            transforms.ToTensor(),
-            transforms.Normalize((0.5,), (0.5,))
-        ])
         
+        if transform is None:
+            self.transform = transforms.Compose([
+                transforms.ToTensor(),  
+                transforms.Resize((32, 32)),  
+                transforms.Normalize((0.5,), (0.5,))  
+            ])
+        else:
+            self.transform = transform
+
     def __len__(self):
         return len(self.data)
     
     def __getitem__(self, idx):
+        # Get the sample
         sample = self.data[idx]
         label = self.labels[idx]
-        if self.transform:
-            sample = self.transform(sample)
+        
+        # Ensure sample is on CPU and convert to numpy if it's a tensor
+        if isinstance(sample, torch.Tensor):
+            sample = sample.cpu().numpy()
+            
+        # Convert to PIL Image (mode 'L' for grayscale)
+        sample_pil = Image.fromarray(sample.astype('uint8'), mode='L')
+
+        sample = self.transform(sample_pil)
 
         return sample, label
-    
+
