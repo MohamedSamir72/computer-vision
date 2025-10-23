b8534cb9426d53de385b9bbd64123bbc6caa2db1
MohamedSamir72 <mosamir9792@gmail.com>
2025-10-23 19:37:45 +0300

    Add recon_loss_weight and kl_divergence_loss_weight

--- variational_Autoencoder/config.py
+++ variational_Autoencoder/config.py
@@ -5,4 +5,7 @@ class Config:
     LEARNING_RATE: float = 0.001
     BATCH_SIZE: int = 64
     EPOCHS: int = 50
+    DEVICE: str = 'cuda'
+    RECONSTRUCTION_LOSS_WEIGHT: float = 1.0
+    KLD_LOSS_WEIGHT: float = 0.1
     
