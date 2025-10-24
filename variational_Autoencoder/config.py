7763a9e60a4bd38e89eef4d9fb7c46b982eaeddd
MohamedSamir72 <mosamir9792@gmail.com>
2025-10-24 23:33:04 +0300

    Modify the hyperparameters

--- variational_Autoencoder/config.py
+++ variational_Autoencoder/config.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 class Config:
     LEARNING_RATE: float = 0.001
     BATCH_SIZE: int = 64
-    EPOCHS: int = 10
+    EPOCHS: int = 30
     DEVICE: str = 'cuda'
     RECONSTRUCTION_LOSS_WEIGHT: float = 1.0
     KLD_LOSS_WEIGHT: float = 1.0
